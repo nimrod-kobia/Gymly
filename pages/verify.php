1c73d1d1fa19ba04db7fad402b40785a1e2df377
Ryan <ryankagua@gmail.com>
2025-09-21 23:08:17 +0300

    Add verifyForm to Forms.php and verify page

--- pages/verify.php
+++ pages/verify.php
@@ -1,18 +1,5 @@
 <?php
 require_once "../autoload.php";
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $inputCode = $_POST['code'] ?? '';
-    if ($inputCode === $_SESSION['verification_code']) {
-        echo "Verification successful. Account activated.";
-    } else {
-        echo "Invalid verification code.";
-    }
-}
-?>
-<form method="POST">
-    <label>Enter Verification Code:</label>
-    <input type="text" name="code" required>
-    <button type="submit">Verify</button>
-</form>
+include '../template/layout.php';
+$form = new Forms();
+echo $form->verifyForm();
